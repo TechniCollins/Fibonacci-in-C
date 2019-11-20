54277f7b99ccdc92318e05e6e972e38d3845afb3
TechniCollins <upendocollins12@gmail.com>
2019-11-20 04:11:00 +0300

    Added new function to get fibonacci in another way

--- fib.c
+++ fib.c
@@ -1,10 +1,8 @@
 /*
 TITLE:        Fibonacci series in C
-
 Contributor:  Collins Upendo, Alias: TechniCollins
               University of Nairobi
               2019
-
 Initialize a to 0 and b to 1 because 
 the first two terms of the series are 
 0 and 1 respectively. c adds the two numbers
@@ -13,7 +11,8 @@ n will hold value from user
 */
 #include <stdio.h>
 int fib[100]; //carries up to the first 100 terms of the fibonacci series
-int tracker = 1;//Tracker is a global function because it will be used in more than one function
+int fib2[100];
+int tracker = 1, counter = 1;//Tracker and counter are global variables because it will be used in more than one function
 int fibonnacci(int n)
 {
 
@@ -28,14 +27,49 @@ while(tracker < n){
 }
 return a;
 }
+
+int fibmax(int num)
+{
+    int d=0, e=1, f;
+    while(f < num)
+    {
+        f = d+e;
+        d = e;
+        e = f;
+        counter++;
+        fib2[counter-1] = f;
+    }
+    
+}
+
 int main()
 {
-    int n, i;
+    int n, i, num, k;
+    int response = -20;
+    printf("What do you need?\nEnter 1: Fibonacci upto n terms\nEnter 2: Fibonacci upto a certain number\n");
+    scanf("%d", &response);
+    if(response == 1)
+    {
     printf("Enter number of terms\n");
     scanf("%d", &n);
     fibonnacci(n);
     for(i = 0; i<tracker; i++)
     {
         printf("%d, ", fib[i]);
+    }
+    }
+    else if(response == 2)
+    {
+        printf("Enter maximum number\n");
+        scanf("%d", &num);
+        fibmax(num);
+        for(k = 0; k<counter; k++)
+        {
+            printf("%d, ", fib2[k]);
+        }
+    }
+    else
+    {
+        printf("Invalid choice");
     }
 }
